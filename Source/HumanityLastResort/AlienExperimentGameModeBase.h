86febd7a930b57be850e8b98d3569e514f88cd9b
khaled elsayed <59780800+khaled71612000@users.noreply.github.com>
2022-05-23 15:27:55 +0200

    Project Rename

---
+++ Source/HumanityLastResort/AlienExperimentGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AlienExperimentGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HUMANITYLASTRESORT_API AAlienExperimentGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

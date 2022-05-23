fdc058c64705d7f8bdfd5554713269f995e38f19
ShahendaWafa <62213542+ShahendaWafa@users.noreply.github.com>
2022-05-23 16:25:37 +0200

    Changed repo name

--- Source/HumanityLastResort/HumanityLastResortGameModeBase.h
+++ Source/HumanityLastResort/HumanityLastResortGameModeBase.h
@@ -4,13 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
-#include "AlienExperimentGameModeBase.generated.h"
+#include "HumanityLastResortGameModeBase.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class HUMANITYLASTRESORT_API AAlienExperimentGameModeBase : public AGameModeBase
+class HUMANITYLASTRESORT_API AHumanityLastResortGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	

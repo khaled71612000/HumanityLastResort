82cd2ad3e82116105251b1fcbe2ab54cac72fa73
ShahendaWafa <62213542+ShahendaWafa@users.noreply.github.com>
2022-06-06 15:18:50 +0200

    Created BuildingSubsystem

---
+++ Source/HumanityLastResort/Public/Buildings/BuildingSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "BuildingSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HUMANITYLASTRESORT_API UBuildingSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+};

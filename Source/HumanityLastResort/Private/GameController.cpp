0da26825dd47d996b6c5beaef4e712fbff36ba49
khaled elsayed <59780800+khaled71612000@users.noreply.github.com>
2022-06-14 09:02:30 +0200

    kz

--- Source/HumanityLastResort/Private/GameController.cpp
+++ Source/HumanityLastResort/Private/GameController.cpp
@@ -24,44 +24,40 @@ void AGameController::BeginPlay()
 	SelectionSubSystem = GetWorld()->GetSubsystem<USelectionSubsystem>();
 	FoundGrid = UGameplayStatics::GetActorOfClass(GetWorld(), ANewGrid::StaticClass());
 
+	if (FoundGrid) {
+		GridPtr = Cast<ANewGrid>(FoundGrid);
+	}
 }
 void AGameController::Tick(float dt) {
 	Super::Tick(dt);
+	GetCurrentMousePosition();
+	GetCurrentBuilding(intersect);
+}
 
-	FVector start, dir;
+void AGameController::GetCurrentMousePosition()
+{
+
 	DeprojectMousePositionToWorld(start, dir);
-	const FVector end = start + dir * 10000.0f;
-	const FPlane plane{
-	{FVector::ZeroVector},
-	{0.f,0.f,1.f}
-	};
-	 FVector intersect = FMath::LinePlaneIntersection(start, end, plane);
-	 intersect.Z = 100;
+	end = start + dir * 10000.0f;
 
-	FHitResult Hit;
-	FCollisionQueryParams TraceParams;
+	intersect= FMath::LinePlaneIntersection(start, end, plane);
+	intersect.Z = 100;
+}
 
-	bool bHit = GetWorld()->LineTraceSingleByChannel(Hit, start, end, ECC_Visibility, TraceParams);
-
+void AGameController::GetCurrentBuilding(const FVector& intersectPoint)
+{
 	TArray<AActor*> actorPtrs;
 
-	UGameplayStatics::GetAllActorsWithInterface(GetWorld(), UPlacementInterface::StaticClass(), actorPtrs);
-
-	ANewGrid* GridPtr;
+	UGameplayStatics::GetAllActorsWithInterface(GetWorld(),
+		UPlacementInterface::StaticClass(), actorPtrs);
 
 	if (actorPtrs.Num() != 0) {
 		for (AActor* actor : actorPtrs) {
 			IPlacementInterface* current = Cast<IPlacementInterface>(actor);
 			if (current) {
-				if (FoundGrid) {
-					GridPtr = Cast<ANewGrid>(FoundGrid);
-					if (GridPtr) {
-						if (bHit) {
-							HitBuilding = Hit;
-							GridPtr->GetClosestPosition(intersect);
-							current->MouseMove(GridPtr->GetClosestPosition(intersect));
-						}
-					}
+				if (GridPtr) {
+					GridPtr->GetClosestPosition(intersectPoint);
+					current->MouseMove(GridPtr->GetClosestPosition(intersectPoint));
 				}
 			}
 		}
@@ -76,8 +72,12 @@ void AGameController::OnLeftMouseRelease()
 
 void AGameController::OnLeftMouseClicked()
 {
-	if(HitBuilding.GetActor())
-	SelectionSubSystem->TrySelect(HitBuilding);
+	FHitResult Hit;
+	FCollisionQueryParams TraceParams;
+	bool bHit = GetWorld()->LineTraceSingleByChannel(Hit, start, end, ECC_Visibility, TraceParams);
+
+	if (Hit.GetActor())
+		SelectionSubSystem->TrySelect(Hit);
 }
 
 void AGameController::SetupInputComponent()

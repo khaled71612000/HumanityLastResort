6d1b422f4917def09a292b890b87e7ab618d1357
khaled elsayed <59780800+khaled71612000@users.noreply.github.com>
2022-06-23 09:46:48 +0200

    Revert "Merge branch 'Last7.3'"
    This reverts commit d1ea66bbf04164644cb118d4bab026b5174c4d12, reversing
    changes made to 2350ddaa7c23aaae0f6f921f47d2cef7528f39b9.

--- Source/HumanityLastResort/HumanityLastResort.Build.cs
+++ Source/HumanityLastResort/HumanityLastResort.Build.cs
@@ -8,7 +8,7 @@ public class HumanityLastResort : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","SlateCore","Slate", "NavigationSystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
--- Source/HumanityLastResort/Private/Buildings/CandyShop.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Buildings/CandyShop.h"
-
-ACandyShop::ACandyShop()
-{
-	BuildingType = CandyShop;
-}

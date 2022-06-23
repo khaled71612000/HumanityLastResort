d4da289e2b8bab4b8fadc9802f26fe88073b3f1d
khaled elsayed <59780800+khaled71612000@users.noreply.github.com>
2022-06-23 09:33:30 +0200

    clear

--- Source/HumanityLastResort/Private/Buildings/Club.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Buildings/Club.h"
-
-AClub::AClub()
-{
-	BuildingType = Club;
-}
--- Source/HumanityLastResort/Private/Buildings/JuiceShop.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Buildings/JuiceShop.h"
-
-AJuiceShop::AJuiceShop()
-{
-	BuildingType = JuiceShop;
-}
